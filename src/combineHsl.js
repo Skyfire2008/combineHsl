57b77fd0fcd095f1d1aafbaf37d3b3e927aeac10
Leonid Darovskikh <skyfire2008@gmail.com>
2017-02-15 20:04:10 +0100

    added image size selection mode and check, that all images are selected

--- src/combineHsl.js
+++ src/combineHsl.js
@@ -8,29 +8,40 @@ var resultCanvas;
 var wrapModeSel;
 var wrapMode=grafix.WrapMode.clamp;
 
+var imgSizeModeSel;
+var imgSizeMode=Math.max;
+
 var hImg=new Image();
 var sImg=new Image();
 var lImg=new Image();
 
 var hCanvas=document.createElement("canvas");
+hCanvas.loaded=false;
 var sCanvas=document.createElement("canvas");
+sCanvas.loaded=false;
 var lCanvas=document.createElement("canvas");
+lCanvas.loaded=false;
 
 var readFile=function(input, image, canvas){
 
-	fr=new FileReader();
+	if(input.files.length>0){ //check, that the file was selected
 
-	fr.onload=function(e){
-		var target=e.target;
-		image.src=target.result;
-		image.onload=new function(){
+		fr = new FileReader();
 
-			canvas.width=image.naturalWidth;
-			canvas.height=image.naturalHeight;
-			canvas.getContext("2d").drawImage(image, 0, 0);
+		fr.onload = function (e) {
+			var target = e.target;
+			image.src = target.result;
+			image.onload = new function () {
+
+				canvas.width = image.naturalWidth;
+				canvas.height = image.naturalHeight;
+				canvas.loaded = true;
+				canvas.getContext("2d").drawImage(image, 0, 0);
+			};
 		};
-	};
-	fr.readAsDataURL(input.files[0]);
+		fr.readAsDataURL(input.files[0]);
+
+	}
 };
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -40,26 +51,32 @@ document.addEventListener("DOMContentLoaded", function(){
 	applyBtn=document.getElementById("applyBtn");
 	applyBtn.onclick=function(){
 
-		resultCanvas.width=Math.max(hCanvas.width, sCanvas.width, lCanvas.width);
-		resultCanvas.height=Math.max(hCanvas.height, sCanvas.height, lCanvas.height);
-		
-		var resultBmd=new grafix.BitmapData(resultCanvas);
-		var hBmd=new grafix.BitmapData(hCanvas);
-		var sBmd=new grafix.BitmapData(sCanvas);
-		var lBmd=new grafix.BitmapData(lCanvas);
-		
-		for(var x=0; x<resultCanvas.width; x++){
-			for(var y=0; y<resultCanvas.height; y++){
+		if(hCanvas.loaded && sCanvas.loaded && lCanvas.loaded) {
 
-				var h=(hBmd.getRGBA(x, y, wrapMode)).toHSL().h;
-				var s=(sBmd.getRGBA(x, y, wrapMode)).toHSL().s;
-				var l=(lBmd.getRGBA(x, y, wrapMode)).toHSL().l;
+			resultCanvas.width = imgSizeMode(hCanvas.width, sCanvas.width, lCanvas.width);
+			resultCanvas.height = imgSizeMode(hCanvas.height, sCanvas.height, lCanvas.height);
 
-				resultBmd.setRGBA(x, y, wrapMode, new grafix.HSL(h, s, l).toRGBA());
+			var resultBmd = new grafix.BitmapData(resultCanvas);
+			var hBmd = new grafix.BitmapData(hCanvas);
+			var sBmd = new grafix.BitmapData(sCanvas);
+			var lBmd = new grafix.BitmapData(lCanvas);
+
+			for (var x = 0; x < resultCanvas.width; x++) {
+				for (var y = 0; y < resultCanvas.height; y++) {
+
+					var h = (hBmd.getRGBA(x, y, wrapMode)).toHSL().h;
+					var s = (sBmd.getRGBA(x, y, wrapMode)).toHSL().s;
+					var l = (lBmd.getRGBA(x, y, wrapMode)).toHSL().l;
+
+					resultBmd.setRGBA(x, y, wrapMode, new grafix.HSL(h, s, l).toRGBA());
+				}
 			}
-		}
 
-		resultBmd.update();
+			resultBmd.update();
+
+		}else{
+			alert("Please load all images first!");
+		}
 		
 	};
 
@@ -80,6 +97,15 @@ document.addEventListener("DOMContentLoaded", function(){
 			wrapMode=grafix.WrapMode.clamp;
 		}else{
 			wrapMode=grafix.WrapMode.mirror;
+		}
+	});
+
+	imgSizeModeSel=document.getElementById("imgSizeModeSel");
+	imgSizeModeSel.addEventListener("change", function(){
+		if(imgSizeModeSel.value=="min"){
+			imgSizeMode=Math.min;
+		}else{
+			imgSizeMode=Math.max;
 		}
 	});
 
